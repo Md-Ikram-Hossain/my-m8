1dcca5b784d8c1dbc6007433e5e37b417f2f2ee8
ikramiiss <ikramhossian22@gmail.com>
2025-12-10 20:55:35 +0600

    price section are completed

--- src/components/PriceOption/PriceOption.jsx
+++ src/components/PriceOption/PriceOption.jsx
@@ -1,3 +1,4 @@
+import Feature from "../Feature/Feature";
 
 
 const PriceOption = ({option}) => {
@@ -5,12 +6,18 @@ const PriceOption = ({option}) => {
     const {name, price, features}= option;
 
     return (
-        <div className="bg-blue-500 rounded-md p-4 text-white">
+        <div className="bg-blue-500 rounded-md p-4 flex flex-col text-white">
           <h2 className="text-center">
               <span className="text-4xl font-extrabold">{price}</span>
             <span className="text-2xl">/mon</span>
           </h2>
           <h4 className="text-3xl text-center my-8">{name}</h4>
+         <div className="pl-6 grow">
+           {
+            features.map((feature, idx) => <Feature key={idx} feature={feature}></Feature>)
+          }
+         </div>
+         <button className="w-full mt-12 bg-green-500 rounded-lg py-2 font-bold hover:bg-green-900">Buy Now</button>
         </div>
     );
 };
